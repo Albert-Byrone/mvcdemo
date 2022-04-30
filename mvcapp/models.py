b8113986c46ba369b62c7efe5d96804159646693
Albert Byrone <albertbyrone1677@gmail.com>
2022-04-30 16:17:50 +0300

    add the student and module models

--- mvcapp/models.py
+++ mvcapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Module(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    fullname = models.CharField(max_length=200)
+    student_number = models.CharField(max_length=200)
+    phonenumber = models.CharField(max_length=200)
+    classname = models.ForeignKey(Module, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.fullname
