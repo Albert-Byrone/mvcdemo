457b28b29390c57423040a05081794ffc444bdb5
Albert Byrone <albertbyrone1677@gmail.com>
2022-04-30 16:20:25 +0300

    add a function that gets all the student object from the database

--- mvcapp/views.py
+++ mvcapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def student_list(request):
+    student_list = Student.objects.all()
+    return render(request, 'list.html' , context={ 'student_list': student_list})
+
