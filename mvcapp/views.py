2c3ca3f93072bffca3f579342b6dd03c5e8e3472
Albert Byrone <albertbyrone1677@gmail.com>
2022-04-30 16:21:08 +0300

    add a function to create and update the user instance

--- mvcapp/views.py
+++ mvcapp/views.py
@@ -5,3 +5,22 @@ def student_list(request):
     student_list = Student.objects.all()
     return render(request, 'list.html' , context={ 'student_list': student_list})
 
+def add_student(request, id=0):
+    if request.method == 'GET':
+        if id ==0:
+            form = StudentForm()
+        else:
+            student = Student.objects.get(pk=id)
+            form = StudentForm(instance=student)
+        return render(request, 'add.html',{ 'form': form})
+    else:
+        if id == 0:
+            form = StudentForm(request.POST)
+        else:
+            student = Student.objects.get(pk=id)
+            form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+        return redirect('list')
+
+
