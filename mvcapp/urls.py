427f79da1be3b1871684ab1da22c8b0a9e9b4fd2
Albert Byrone <albertbyrone1677@gmail.com>
2022-04-30 17:42:19 +0300

    update the app urls

--- mvcapp/urls.py
+++ mvcapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.add_student, name='add-student'),
+    path('add-student/', views.add_student, name='add-student'),
     path('<int:id>/', views.add_student, name='student-update'),
-    path('list/', views.student_list, name='list'),
+    path('', views.student_list, name='list'),
     path('delete/<int:id>', views.student_delete, name='delete-student')
 ]
